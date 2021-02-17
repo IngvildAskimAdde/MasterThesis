cda5cc8250da5ee86685a6e3b5d81d9da48798ed
Ingvild Askim Adde <ingvilad@stud.ntnu.no>
2021-02-17 16:32:08 +0100

    Removing commented out parts.

--- Stratify.py
+++ Stratify.py
@@ -11,23 +11,7 @@ data_LARC = pd.read_excel("/Volumes/HARDDISK/MasterThesis/Excel_data/150701 Klin
 def categorize(data):
 
     category = np.zeros(len(data))
-    """
-    for index, row in data.iterrows():
-        if row['Kjønn'] == 'M' and row['Stage'] == 2:
-            category[index] = 1
-        elif row['Kjønn'] == 'M' and row['Stage'] == 3:
-            category[index] = 2
-        elif row['Kjønn'] == 'M' and row['Stage'] == 4:
-            category[index] = 3
-        elif row['Kjønn'] == 'K' and row['Stage'] == 2:
-            category[index] = 4
-        elif row['Kjønn'] == 'K' and row['Stage'] == 3:
-            category[index] = 5
-        elif row['Kjønn'] == 'K' and row['Stage'] == 4:
-            category[index] = 6
-        else:
-            print('Patient '+ str(index)+ ' unknown category')       
-    """
+
     for index, row in data.iterrows():
         if row['Kjønn'] == 'M' and row['Stage'] == 2 and row['DWI'] == 'YES':
             category[index] = 1
@@ -62,19 +46,8 @@ def categorize(data):
 
 
 def plot_distribution(category, title):
-    """
-    stage = FixedFormatter(['T2', 'T3', 'T4'])
-    men = [(category == 1).sum(), (category == 2).sum(), (category == 3).sum()]
-    women = [(category == 4).sum(), (category == 5).sum(), (category == 6).sum()]
 
-    stage = FixedFormatter(['T2', 'T3', 'T4'])
-    men_with_dwi = [(category == 1).sum(), (category == 3).sum(), (category == 5).sum()]
-    women_with_dwi = [(category == 7).sum(), (category == 9).sum(), (category == 11).sum()]
-    men_without_dwi = [(category == 2).sum(), (category == 4).sum(), (category == 6).sum()]
-    women_without_dwi = [(category == 8).sum(), (category == 10).sum(), (category == 12).sum()]
-    """
-
-    stage = FixedFormatter(['T2 \n (DWI available)', 'T2 ', 'T3 \n (DWI available)', 'T3 \n', 'T4 \n (DWI available)', 'T4 \n'])
+    stage = FixedFormatter(['T2 \n (DWIa)', 'T2 \n (DWIna) ', 'T3 \n (DWIa)', 'T3 \n (DWIna)', 'T4 \n (DWIa)', 'T4 \n (DWIna)'])
     men = [(category == 1).sum(), (category == 2).sum(), (category == 3).sum() ,(category == 4).sum(), (category == 5).sum(), (category == 6).sum()]
     women = [(category == 7).sum(), (category == 8).sum(), (category == 9).sum(), (category == 10).sum(), (category == 11).sum(), (category == 12).sum()]
 
@@ -86,20 +59,14 @@ def plot_distribution(category, title):
 
     matplotlib.rcParams.update({'font.size': 25})
     matplotlib.rcParams['font.family'] = "serif"
-    matplotlib.rcParams.update({'xtick.labelsize': 15})
+    matplotlib.rcParams.update({'xtick.labelsize': 20})
 
     fig = plt.figure(figsize=(11,8))
     ax = fig.gca()
 
-    rects1 = ax.bar(x-width/2, men, width, color='#2E7578', label='Men')
-    rects2 = ax.bar(x+width/2, women, width, color='#97D2D4', label='Women')
+    rects1 = ax.bar(x-width/2, men, width, color='#00509E', label='Men')
+    rects2 = ax.bar(x+width/2, women, width, color='#9ECFFF', label='Women')
 
-    """
-    rects1 = ax.bar(x-width/4, men_with_dwi, width, color='#2E7578', label='Men DWI')
-    rects2 = ax.bar(x-width/2, women_with_dwi, width, color='#97D2D4', label='Women DWI')
-    rects3 = ax.bar(x+width/2, men_without_dwi, width, label='Men')
-    rects4 = ax.bar(x+width/4, women_without_dwi, width, label='Women')
-    """
     ax.set_ylabel(r'Number of patients')
     ax.set_xlabel(r'Stage') #, fontsize=20)
     ax.xaxis.set_major_formatter(stage)
@@ -121,23 +88,7 @@ def plot_distribution(category, title):
                     xytext=(0, 3),  # 3 points vertical offset
                     textcoords="offset points",
                     ha='center', va='bottom')
-    """
-    for rect in rects3:
-        height = rect.get_height()
-        ax.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
 
-    for rect in rects4:
-        height = rect.get_height()
-        ax.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
-    """
     bottom, top = ax.get_ylim()
     ax.set_ylim(bottom, top+3)
     ax.legend()
@@ -222,9 +173,9 @@ def convert_kFoldDictArray_to_set(dictionary):
 def create_traditionalSplit_dict(train, val, test, smaller_dimensions=None):
 
     patient_dict = {}
-    patient_dict['Train'] = {}
-    patient_dict['Validation'] = {}
-    patient_dict['Test'] = {}
+    patient_dict['train'] = {}
+    patient_dict['val'] = {}
+    patient_dict['test'] = {}
 
     set_small_dimensions = set()
     set_large_dimensions = set()
@@ -237,10 +188,10 @@ def create_traditionalSplit_dict(train, val, test, smaller_dimensions=None):
             set_large_dimensions.add(patient)
 
     if (len(set_small_dimensions)==0):
-        patient_dict['Train']['512'] = [set_large_dimensions]
+        patient_dict['train']['512'] = [set_large_dimensions]
     else:
-        patient_dict['Train']['512'] = [set_large_dimensions]
-        patient_dict['Train']['256'] = [set_small_dimensions]
+        patient_dict['train']['512'] = [set_large_dimensions]
+        patient_dict['train']['256'] = [set_small_dimensions]
 
     set_small_dimensions = set()
     set_large_dimensions = set()
@@ -253,10 +204,10 @@ def create_traditionalSplit_dict(train, val, test, smaller_dimensions=None):
             set_large_dimensions.add(patient)
 
     if (len(set_small_dimensions)==0):
-        patient_dict['Validation']['512'] = [set_large_dimensions]
+        patient_dict['val']['512'] = [set_large_dimensions]
     else:
-        patient_dict['Validation']['512'] = [set_large_dimensions]
-        patient_dict['Validation']['256'] = [set_small_dimensions]
+        patient_dict['val']['512'] = [set_large_dimensions]
+        patient_dict['val']['256'] = [set_small_dimensions]
 
     set_small_dimensions = set()
     set_large_dimensions = set()
@@ -269,19 +220,19 @@ def create_traditionalSplit_dict(train, val, test, smaller_dimensions=None):
             set_large_dimensions.add(patient)
 
     if (len(set_small_dimensions)==0):
-        patient_dict['Test']['512'] = [set_large_dimensions]
+        patient_dict['test']['512'] = [set_large_dimensions]
     else:
-        patient_dict['Test']['512'] = [set_large_dimensions]
-        patient_dict['Test']['256'] = [set_small_dimensions]
+        patient_dict['test']['512'] = [set_large_dimensions]
+        patient_dict['test']['256'] = [set_small_dimensions]
 
     return patient_dict
 
 trainOxy, train_catOxy, train_indexOxy, valOxy, val_catOxy, val_indexOxy, testOxy, test_catOxy, test_indexOxy = traditional_split(patientsOxy, categoryOxy, 17, 16)
 trainLARC, train_catLARC, train_indexLARC, valLARC, val_catLARC, val_indexLARC, testLARC, test_catLARC, test_indexLARC = traditional_split(patientsLARC, categoryLARC, 13, 13)
 
-trainVal_patients_Oxy = np.append(valOxy, trainOxy)
-trainVal_category_Oxy = np.append(val_catOxy, train_catOxy)
-kfold_patients_Oxy = kfold_split(trainVal_patients_Oxy, trainVal_category_Oxy, 5)
+#trainVal_patients_Oxy = np.append(valOxy, trainOxy)
+#trainVal_category_Oxy = np.append(val_catOxy, train_catOxy)
+#kfold_patients_Oxy = kfold_split(trainVal_patients_Oxy, trainVal_category_Oxy, 5)
 
 #This section is needed when there is only one patient with a certain class
 
@@ -308,17 +259,18 @@ val_cat = np.append(val_catOxy, val_catLARC)
 
 test = np.append(testOxy, testLARC)
 test_cat = np.append(test_catOxy, test_catLARC)
-"""
+
 ####### PLOT AND PRINT TRAIN, VALIDATION AND TEST ################
-#plot_distribution(categoryLARC, r'Total dataset')
-#plot_distribution(train_catLARC, r'Training set')
-#plot_distribution(val_catLARC, r'Validation set')
-#plot_distribution(test_catLARC, r'Test set')
+#plot_distribution(category, r'Total dataset')
+#plot_distribution(train_cat, r'Training set')
+#plot_distribution(val_cat, r'Validation set')
+#plot_distribution(test_cat, r'Test set')
 
-print(np.sort(trainLARC))
-print(np.sort(valLARC))
-print(np.sort(testLARC))
+#print(np.sort(train))
+#print(np.sort(val))
+#print(np.sort(test))
 
+"""
 ####### PLOT AND PRINT K-FOLDS ##################################
 plot_distribution(kfold_cat_Oxy['Train0'], r'Total dataset')
 plot_distribution(kfold_cat_Oxy['Validation0'], r'Total dataset')
@@ -349,17 +301,31 @@ print('Validation:', np.sort(kfold_patients_Oxy['Fold5']['Validation5']))
 
 
 #kfold_patients_Oxy = convert_kFoldDictArray_to_set(kfold_patients_Oxy)
-
-
-small_dimensions_patients_Oxy = []
-tradSplit_patients_Oxy = create_traditionalSplit_dict(trainOxy, valOxy, testOxy, smaller_dimensions=small_dimensions_patients_Oxy)
-
-small_dimensions_patients = ['LARC-RRP-011','LARC-RRP-013','LARC-RRP-014','LARC-RRP-015','LARC-RRP-016','LARC-RRP-019']
-tradSplit_patients_LARC = create_traditionalSplit_dict(trainLARC, valLARC, testLARC, smaller_dimensions=small_dimensions_patients)
-
-tradSplit_patients_LARC_Oxy = create_traditionalSplit_dict(train, val, test, smaller_dimensions=small_dimensions_patients)
 """
 
-f = open("Oxy_kFold_patients_dict.txt","w")
-f.write(str(kfold_patients_Oxy))
+#small_dimensions_patients_Oxy = []
+#tradSplit_patients_Oxy = create_traditionalSplit_dict(trainOxy, valOxy, testOxy, smaller_dimensions=small_dimensions_patients_Oxy)
+
+small_dimensions_patients = ['LARC-RRP-011','LARC-RRP-013','LARC-RRP-014','LARC-RRP-015','LARC-RRP-016','LARC-RRP-019']
+#tradSplit_patients_LARC = create_traditionalSplit_dict(trainLARC, valLARC, testLARC, smaller_dimensions=small_dimensions_patients)
+
+tradSplit_patients_LARC_Oxy = create_traditionalSplit_dict(train, val, test, smaller_dimensions=small_dimensions_patients)
+
+
+f = open("LARC_Oxy_tradSplit_patients_dict.txt","w")
+f.write(str(tradSplit_patients_LARC_Oxy))
+f.close()
+
+f = open("LARC_Oxy_tradSplit_category.txt","w")
+f.write('Total set:')
+f.write(str(category))
+f.write('\n')
+f.write('Train split:')
+f.write(str(train_cat))
+f.write('\n')
+f.write('Validation split:')
+f.write(str(val_cat))
+f.write('\n')
+f.write('Test split:')
+f.write(str(test_cat))
 f.close()
