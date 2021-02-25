5b5d9acb372ab6fec63c9ca51fa74a1e004a8e00
Ingvild Askim Adde <ingvilad@stud.ntnu.no>
2021-02-25 17:43:04 +0100

    Added description of functions.

--- Evaluate.py
+++ Evaluate.py
@@ -4,15 +4,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import  matplotlib
 
-data_valfold1 = pd.read_csv('/Volumes/HARDDISK/MasterThesis/Experiments/Oxy/kfold/2d_unet_Oxy_kfold_valFold1/logs.csv')
-data_valfold2 = pd.read_csv('/Volumes/HARDDISK/MasterThesis/Experiments/Oxy/kfold/2d_unet_Oxy_kfold_valFold2/logs.csv')
-data_valfold3 = pd.read_csv('/Volumes/HARDDISK/MasterThesis/Experiments/Oxy/kfold/2d_unet_Oxy_kfold_valFold3/logs.csv')
-data_valfold4 = pd.read_csv('/Volumes/HARDDISK/MasterThesis/Experiments/Oxy/kfold/2d_unet_Oxy_kfold_valFold4/logs.csv')
-data_valfold5 = pd.read_csv('/Volumes/HARDDISK/MasterThesis/Experiments/Oxy/kfold/2d_unet_Oxy_kfold_valFold5/logs.csv')
+data_valfold1 = pd.read_csv('/Volumes/HARDDISK/MasterThesis/Experiments/LARC/kfold/ID_1/logs.csv')
+data_valfold2 = pd.read_csv('/Volumes/HARDDISK/MasterThesis/Experiments/LARC/kfold/ID_2/logs.csv')
+data_valfold3 = pd.read_csv('/Volumes/HARDDISK/MasterThesis/Experiments/LARC/kfold/ID_3/logs.csv')
+data_valfold4 = pd.read_csv('/Volumes/HARDDISK/MasterThesis/Experiments/LARC/kfold/ID_4/logs.csv')
+data_valfold5 = pd.read_csv('/Volumes/HARDDISK/MasterThesis/Experiments/LARC/kfold/ID_5/logs.csv')
 
 dataframes = [data_valfold1, data_valfold2, data_valfold3, data_valfold4, data_valfold5]
 
 def create_dataframe(dataframes_list, colname, maxsize_of_dataframe):
+    """
+    Takes in a list of dataframes and extracts the information from a column given by the parameter colname.
+    The information in the columns with colname is merged into one dataframe, which is returned.
+    """
     dataframe = pd.DataFrame()
     dataframe['epoch'] = np.arange(0,maxsize_of_dataframe)
 
@@ -22,6 +26,10 @@ def create_dataframe(dataframes_list, colname, maxsize_of_dataframe):
     return dataframe
 
 def plot_data(dataframe, yname):
+    """
+    Plots the data given in the dataframe.
+    yname: label of y-axis
+    """
     number_of_epochs = np.size(dataframe, axis=0)
     number_of_plots = np.size(dataframe, axis=1)-1
     x_axis = np.arange(0,number_of_epochs)
@@ -37,12 +45,15 @@ def plot_data(dataframe, yname):
 
     plt.xlabel('Epoch')
     plt.ylabel(yname)
-    plt.title('Validation')
+    plt.title('Training')
     plt.legend()
     fig.tight_layout()
     plt.show()
 
 def calculate_median(dataframe):
+    """
+    Returns a list of median and standard deviation values of the columns in a dataframe.
+    """
     median = []
     std = []
     for col, value in dataframe.iteritems():
@@ -53,7 +64,7 @@ def calculate_median(dataframe):
     return median, std
 
 
-dataframe = create_dataframe(dataframes, 'val_dice', 74)
+dataframe = create_dataframe(dataframes, 'dice', 93)
 plot_data(dataframe, 'Dice')
 median, std = calculate_median(dataframe)
 
