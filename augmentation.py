067cc07b90529125a193d5c90822004dd03a8606
Ingvild Askim Adde <ingvilad@stud.ntnu.no>
2021-03-08 19:41:39 +0100

    Added useful functions to visualize augmentations.

--- augmentation.py
+++ augmentation.py
@@ -1,7 +1,10 @@
 import h5py
-#from deoxys.data.preprocessor import ImageNormalizerPreprocessor
+from deoxys.data.preprocessor import ImageNormalizerPreprocessor
 import matplotlib.pyplot as plt
 from medvis import apply_cmap_with_blend
+from deoxys_image import affine_transform
+from deoxys_image import point_operation
+from deoxys_image import filters
 
 
 def get_images_and_targets(filepath, indice):
@@ -11,16 +14,19 @@ def get_images_and_targets(filepath, indice):
     with h5py.File(filepath, 'r') as f:
         images = f['val/352']['input'][indice]
         targets = f['val/352']['target_an'][indice]
-        #images, targets = ImageNormalizerPreprocessor().transform(images, targets)
+        images, targets = ImageNormalizerPreprocessor().transform(images, targets)
         return images, targets
 
-def plot_single_image(ax, img, contour):
+def plot_single_image(img, contour):
     """
     Plots a single image and contour.
     """
-    ax.imshow(img, 'gray') #, vmin=0, vmax=1
+    plt.figure()
+    ax = plt.subplot(111)
+    ax.imshow(img[0][..., 0], 'gray', vmin=0, vmax=1)
     ax.axis('off')
-    ax.contour(contour[..., 0], 1, levels=[0.5], colors='yellow')
+    ax.contour(contour[0][..., 0], 1, levels=[0.5], colors='yellow')
+    plt.show()
 
 def plot_4_single(images, targets):
     """
@@ -32,7 +38,81 @@ def plot_4_single(images, targets):
 
     plt.show()
 
+def flip(img, contour, axis):
+    transformed_images = img.copy()
+    transformed_targets = contour.copy()
+
+    transformed_images[0] = affine_transform.apply_flip(img[0], axis=axis)
+    transformed_targets[0] = affine_transform.apply_flip(contour[0], axis=axis)
+
+    plot_single_image(transformed_images, transformed_targets)
+
+def rotate(img, contour, degrees):
+    transformed_images = img.copy()
+    transformed_targets = contour.copy()
+
+    transformed_images[0] = affine_transform.apply_affine_transform(img[0], theta=degrees, axis=2)
+    transformed_targets[0] = affine_transform.apply_affine_transform(contour[0], theta=degrees, axis=2)
+
+    plot_single_image(transformed_images, transformed_targets)
+
+def zoom(img, contour, zoom):
+    transformed_images = img.copy()
+    transformed_targets = contour.copy()
+
+    transformed_images[0] = affine_transform.apply_affine_transform(img[0], zoom_factor=zoom)
+    transformed_targets[0] = affine_transform.apply_affine_transform(contour[0], zoom_factor=zoom)
+
+    plot_single_image(transformed_images, transformed_targets)
+
+def shift(img, contour, shift_range):
+    transformed_images = img.copy()
+    transformed_targets = contour.copy()
+
+    transformed_images[0] = affine_transform.apply_affine_transform(img[0], shift=shift_range)
+    transformed_targets[0] = affine_transform.apply_affine_transform(contour[0], shift=shift_range)
+
+    plot_single_image(transformed_images, transformed_targets)
+
+
+def brightness(img, contour, brightness_factor, channel):
+    transformed_images = img.copy()
+    transformed_targets = contour.copy()
+
+    transformed_images[0] = point_operation.change_brightness(img[0], factor=brightness_factor, channel=channel)
+
+    plot_single_image(transformed_images, transformed_targets)
+
+def contrast(img, contour, contrast_factor, channel):
+    transformed_images = img.copy()
+    transformed_targets = contour.copy()
+
+    transformed_images[0] = point_operation.change_brightness(img[0], factor=contrast_factor, channel=channel)
+
+    plot_single_image(transformed_images, transformed_targets)
+
+def noise(img, contour, noise_var, channel):
+    transformed_images = img.copy()
+    transformed_targets = contour.copy()
+
+    transformed_images[0] = point_operation.gaussian_noise(img[0], noise_var=noise_var, channel=channel)
+
+    plot_single_image(transformed_images, transformed_targets)
+
+def blur(img, contour, blur_value, channel):
+    transformed_images = img.copy()
+    transformed_targets = contour.copy()
+
+    transformed_images[0] = filters.gaussian_blur(img[0], sigma=blur_value, channel=channel)
+
+    plot_single_image(transformed_images, transformed_targets)
+
+
+
 path = '/Volumes/HARDDISK/MasterThesis/HDF5_data/traditionalSplit_Oxy.h5'
-indice = [6, 10, 14, 16]
+indice = [10]#, 10, 14, 16]
 images, targets = get_images_and_targets(path, indice)
-plot_4_single(images, targets)
+plot_single_image(images, targets)
+#flip(images, targets, axis=0)
+#rotate(images, targets, degrees=90)
+blur(images, targets, blur_value=0.5, channel=None)
