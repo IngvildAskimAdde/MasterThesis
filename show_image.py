9695be5a8c4ea7f59c755c8160b56cfb9eb0e51c
Ingvild Askim Adde <ingvilad@stud.ntnu.no>
2021-03-22 10:51:35 +0100

    Code for visualizing images

--- show_image.py
+++ show_image.py
@@ -1,12 +1,16 @@
 import ImageViewer as iv
 import SimpleITK as sitk
+import h5py
 
 image = sitk.ReadImage('/Volumes/LaCie/MasterThesis_Ingvild/Oxy_cropped_ZScoreNorm/Oxytarget_91_PRE/T2.nii')
 mask = sitk.ReadImage('/Volumes/LaCie/MasterThesis_Ingvild/Oxy_cropped_ZScoreNorm/Oxytarget_91_PRE/Manual_an.nii')
 
-v = iv.Viewer(view_mode='2', mask_to_show=['a'])
-v.set_image(image, label='image')
-v.set_mask(mask, label='mask')
-v.show()
+#v = iv.Viewer(view_mode='2', mask_to_show=['a'])
+#v.set_image(image, label='image')
+#v.set_mask(mask, label='mask')
+#v.show()
 
-print(sitk.GetArrayFromImage(mask).max())
+#print(sitk.GetArrayFromImage(mask).max())
+
+f = h5py.File('/Volumes/LaCie/MasterThesis_Ingvild/HDF5_data/traditionalSplit_Oxy_new.h5', 'r')
+print(f['train/352']['target_an'][10].max())
