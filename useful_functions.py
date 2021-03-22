6222c4b2d7d98f24af4a3ac9887cfeede3563662
Ingvild Askim Adde <ingvilad@stud.ntnu.no>
2021-03-22 14:35:00 +0100

    File for collecting useful functions for later use.

---
+++ useful_functions.py
@@ -0,0 +1,31 @@
+
+import os
+
+def create_folder(src_main_folder, dst_main_folder, patient_identifier):
+    """
+    Input: Path to source folder, path to destination folder, patient
+    identifier (LARC-RRP or ID)
+
+    Output: New folder with the same structure as source folder
+    """
+
+    patient_list = [i for i in os.listdir(src_main_folder) if i.startswith(patient_identifier)]
+
+    if patient_identifier == 'LARC-RRP':
+        scan_time_point_folder = 'MRS1'
+        for patient in patient_list:
+            os.makedirs(os.path.join(dst_main_folder, patient, scan_time_point_folder))
+
+    elif patient_identifier == 'Oxytarget':
+        for patient in patient_list:
+            os.makedirs(os.path.join(dst_main_folder, patient))
+
+    elif patient_identifier == 'ID':
+        for patient in patient_list:
+            os.makedirs(os.path.join(dst_main_folder, patient))
+
+    else:
+        print('No valid patient identifier')
+
+
+create_folder(r'/Volumes/Untitled 1/Ingvild_Oxytarget', r'/Volumes/LaCie/MasterThesis_Ingvild/Oxy_cropped_corrected', 'Oxytarget')
