67848ef1ee79bf3a60549f95dfea59c48af641b4
Ingvild Askim Adde <ingvilad@stud.ntnu.no>
2021-03-24 22:14:24 +0100

    Adding array from image function, and plot function

--- useful_functions.py
+++ useful_functions.py
@@ -2,7 +2,10 @@
 import os
 import ImageViewer as iv
 import SimpleITK as sitk
+import numpy as np
+import matplotlib.pyplot as plt
 
+###### Folders and path functions ###########################
 def create_folder(src_main_folder, dst_main_folder, patient_identifier):
     """
     Input: Path to source folder, path to destination folder, patient
@@ -29,6 +32,7 @@ def create_folder(src_main_folder, dst_main_folder, patient_identifier):
     else:
         print('No valid patient identifier')
 
+
 def create_dst_paths(dst_main_folder):
     """
     Creates a list of destination paths based on the structure of the dst_main_folder path
@@ -47,6 +51,21 @@ def create_dst_paths(dst_main_folder):
     return dst_list
 
 
+def get_array_from_image(path):
+    """
+    Reads an image from the given path, and returns a flatten image-array and image size
+
+    :param path: path to image
+    :return: flatten image array and image size
+    """
+    image = sitk.ReadImage(path)
+    array = sitk.GetArrayFromImage(image)
+    image_size = np.shape(array)
+    array = array.flatten()
+    return array, image_size
+
+
+###### Visualization of images ##############################
 def show_image_interactive(image_path, mask_path, view_mode):
 
     image = sitk.ReadImage(image_path)
@@ -56,3 +75,40 @@ def show_image_interactive(image_path, mask_path, view_mode):
     v.set_image(image, label='image')
     v.set_mask(mask, label='mask')
     v.show()
+
+def plot_2x2(image1_path, image2_path, image3_path, image4_path, slice_number):
+
+    image1 = sitk.ReadImage(image1_path)
+    image2 = sitk.ReadImage(image2_path)
+    image3 = sitk.ReadImage(image3_path)
+    image4 = sitk.ReadImage(image4_path)
+
+    image1_array = sitk.GetArrayFromImage(image1)
+    image2_array = sitk.GetArrayFromImage(image2)
+    image3_array = sitk.GetArrayFromImage(image3)
+    image4_array = sitk.GetArrayFromImage(image4)
+
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=1, ncols=4, figsize=(8, 3),
+                                        sharex=True, sharey=True)
+    for aa in (ax1, ax2, ax3, ax4):
+        aa.set_axis_off()
+
+    ax1.imshow(image1_array[slice_number], cmap='gray')
+    ax1.set_title('Source')
+    ax2.imshow(image2_array[slice_number], cmap='gray')
+    ax2.set_title('Reference')
+    ax3.imshow(image3_array[slice_number], cmap='gray')
+    ax3.set_title('Matched')
+    ax4.imshow(image4_array[slice_number], cmap='gray')
+    ax4.set_title('Matched')
+
+    plt.tight_layout()
+    plt.show()
+
+
+im1_Oxy = '/Volumes/LaCie/MasterThesis_Ingvild/Oxy_cropped/Oxytarget_90_PRE/T2.nii'
+im1_LARC = '/Volumes/LaCie/MasterThesis_Ingvild/LARC_cropped/LARC-RRP-035/image.nii'
+im2_Oxy = '/Volumes/LaCie/MasterThesis_Ingvild/Oxy_cropped_ZScoreNorm/Oxytarget_90_PRE/T2.nii'
+im2_LARC = '/Volumes/LaCie/MasterThesis_Ingvild/LARC_cropped_ZScoreNorm/LARC-RRP-035/image.nii'
+
+plot_2x2(im1_Oxy, im1_LARC, im2_Oxy, im2_LARC, 20)
