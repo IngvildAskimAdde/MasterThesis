790b3ad5c20979b3ba041105eb1699f6989b84e6
Ingvild Askim Adde <ingvilad@stud.ntnu.no>
2021-03-26 08:38:16 +0100

    Evaluating

--- Evaluate.py
+++ Evaluate.py
@@ -5,9 +5,9 @@ import numpy as np
 import  matplotlib
 
 #LARC_ID_20 = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/LARC/LARC_ID_20/logs.csv')
-Oxy_ID_11_new = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/Oxy_new/Oxy_ID_11_new/logs.csv')
+Oxy_ID_13_new = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/Oxy_new/Oxy_ID_13_new/logs.csv')
 #Oxy_ID_8_new = pd.read_csv('/Users/ingvildaskimadde/Desktop/Oxy_ID_8_new/logs.csv')
-#Combined_ID_12 = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/Combined/Combined_ID_12/logs.csv')
+#Combined_ID_2_new = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/Combined_new/Combined_ID_2_new/logs.csv')
 
 #Oxy_ID_11_patient = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/Oxy/Oxy_ID_11/patient.csv')
 #LARC_ID_20_slice = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/LARC/LARC_ID_20/slice.csv')
@@ -96,7 +96,7 @@ def get_data(dataframe, column):
 #median, std = calculate_median(dataframe)
 
 #max_dice, epoch = find_best_epoch(Combined_ID_9)
-max_dice, epoch = find_best_epoch(Oxy_ID_11_new)
+max_dice, epoch = find_best_epoch(Oxy_ID_13_new)
 #mean, median = get_data(LARC_ID_7_patient, 'f1_score')
 #mean = get_data(LARC_ID_10_slice, 'f1_score')
 
