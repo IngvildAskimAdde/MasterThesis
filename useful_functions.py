cf598e2996f3b4d255960c64590d83d8781c7b61
Ingvild Askim Adde <ingvilad@stud.ntnu.no>
2021-04-06 13:26:26 +0200

    Added function for creating dataframe, and violin plot.

--- useful_functions.py
+++ useful_functions.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib
 from matplotlib.lines import Line2D
+import pandas as pd
 
 matplotlib.rcParams.update({'font.size': 25})
 matplotlib.rcParams['font.family'] = "serif"
@@ -58,6 +59,7 @@ def create_dst_paths(dst_main_folder):
     return dst_list
 
 
+####### Get data functions ##################################
 def get_array_from_image(path):
     """
     Reads an image from the given path, and returns a flatten image-array and image size
@@ -70,6 +72,99 @@ def get_array_from_image(path):
     image_size = np.shape(array)
     array = array.flatten()
     return array, image_size
+
+def dataframe_of_f1scores(excel_path: str, sheet_name: str, filetype: list, range_ids, patient_ids=False):
+    """
+    The function takes in an excel file and creates a list of paths to the wanted file(s) (filetype).
+    The wanted files are then converted to dataframes, and a singel dataframe containing all the
+    'f1_scores' in the wanted files is returned.
+
+
+    excel_path: path to excel file containing the paths to folders with experiment results
+    sheet_name: name of the excel sheet you want to create a dataframe of
+    filetype: filename of the file you want to collect from the result folder
+
+    Returns a dataframe with all of f1_scores for the filetype for a given dataset.
+    """
+    dataframe = pd.read_excel(excel_path, sheet_name=sheet_name)  # Create dataframe of excel sheet
+    paths = list(dataframe['Result path'])  # Get a list of folderpaths were the results are saved
+    learning_rates = list(dataframe['Learning rate'])
+    loss_functions = list(dataframe['Loss function'])
+    IDs = list(dataframe['Experiment ID'])
+    file_paths_352 = []  # List of paths for images with dimension 352x352
+    file_paths_256 = []  # List of paths for images with dimension 256x256
+    df = pd.DataFrame()
+
+    for file in filetype:
+
+        for i in range_ids:
+
+            if (dataframe['Dataset'][
+                0] == 'Oxytarget'):  # If the dataset is Oxytarget then all the images have the same dimensions.
+                paths[i] = paths[i] + '/' + file  # List of paths for the patient.csv files
+                if os.path.isfile(paths[i]):  # Check if the file path exists
+                    if df.empty:  # Check if the dataframe is empty
+                        df = pd.read_csv(paths[i])  # Add result to dataframe
+                        df.rename(columns={'f1_score': '{:.0e}'.format(learning_rates[i]) + '+' + loss_functions[i]},
+                                  inplace=True)  # Rename column
+                        # df = df.rename(columns={'f1_score': IDs[i]})
+                    else:
+                        # If the dataframe is not empty, add columns with the score from the other results
+                        df_temp = pd.read_csv(paths[i])
+                        df = df.join(df_temp.set_index('patient_ids'), on='patient_ids')
+                        df.rename(columns={'f1_score': '{:.0e}'.format(learning_rates[i]) + '+' + loss_functions[i]},
+                                  inplace=True)
+                        # df = df.rename(columns={'f1_score': IDs[i]})
+
+
+
+            else:  # For other datasets the images are of two different dimensions
+                if file.endswith('352.csv'):
+                    file_paths_352.append(str(paths[i]) + '/' + file)
+                else:
+                    file_paths_256.append(str(paths[i]) + '/' + file)
+
+    if not dataframe['Dataset'][0] == 'Oxytarget':
+        for i in range(len(file_paths_352)):
+            if os.path.isfile(file_paths_352[i]) and os.path.isfile(file_paths_256[i]):
+
+                if df.empty:  # Check if the dataframe is empty
+                    df_352 = pd.read_csv(file_paths_352[i])
+                    df_256 = pd.read_csv(file_paths_256[i])
+                    df = df_352.append(df_256, ignore_index=True)
+                    df.rename(columns={
+                        'f1_score': '{:.0e}'.format(learning_rates[range_ids[i]]) + '+' + loss_functions[range_ids[i]]},
+                              inplace=True)  # Rename column
+                    # df = df.rename(columns={'f1_score': IDs[i]})
+
+                else:
+                    # If the dataframe is not empty, add columns with the score from the other results
+                    df_352 = pd.read_csv(file_paths_352[i])
+                    df_256 = pd.read_csv(file_paths_256[i])
+                    df_temp = df_352.append(df_256, ignore_index=True)
+                    df = df.join(df_temp.set_index('patient_ids'), on='patient_ids')
+                    df.rename(columns={
+                        'f1_score': '{:.0e}'.format(learning_rates[range_ids[i]]) + '+' + loss_functions[range_ids[i]]},
+                              inplace=True)  # Rename column
+                    # df = df.rename(columns={'f1_score': IDs[i]})
+
+    if not patient_ids:
+        df = df.drop(['patient_ids'], axis=1)
+
+    return df
+
+
+def swap_columns(df, c1, c2):
+    """
+    The function swaps the position of two columns (c1 and c2) in a dataframe (df).
+    Returns new dataframe with swapped columns.
+    """
+    df['temp'] = df[c1]
+    df[c1] = df[c2]
+    df[c2] = df['temp']
+    df.drop(columns=['temp'], inplace=True)
+    df = df.rename(columns={c1:c2, c2:c1})
+    return df
 
 
 ###### Visualization of images ##############################
@@ -92,7 +187,7 @@ def show_image(image_path, mask_path, slice_number):
 
     plt.figure(figsize=(11,8))
     plt.imshow(image_array[slice_number], cmap='gray')#, vmin=-2, vmax=4)
-    #plt.plot(mask_array[slice_number])
+    plt.imshow(mask_array[slice_number], cmap='gray', alpha=0.5)
     plt.axis('off')
     plt.show()
 
@@ -141,7 +236,7 @@ def plot_pixel_distribution(df):
     :param df: dataframe with image paths
     :return: a plot of the distribution of pixel intensities in the images given by the dataframe
     """
-    plt.figure(figsize=(14, 11))
+    plt.figure(figsize=(14, 8))
 
     for i in range(len(df['imagePaths'])):
         print('Plotting:', df['imagePaths'][i])
@@ -183,6 +278,39 @@ def plot_matched_images(source_image_path, reference_image_path, matched_image_p
     ax3.imshow(matched_image_array[slice_number], cmap='gray')
     #ax3.set_title('Matched Image')
 
+    plt.tight_layout()
+    plt.show()
+
+
+def violinplot(dataframe, fontsize, labelsize, title, colors):
+
+    plt.figure(figsize=(11, 8))
+
+    matplotlib.rcParams.update({'font.size': fontsize})
+    matplotlib.rcParams['font.family'] = "serif"
+    matplotlib.rcParams.update({'xtick.labelsize': labelsize})
+    sns.violinplot(x=dataframe['Parameters'], y=dataframe['value'], palette=colors)
+    plt.xlabel(None)
+    plt.ylabel('DSC')
+    plt.title(title)
+    plt.xticks(rotation=30)
+    plt.ylim(-0.35, 1.15)
+    plt.tight_layout()
+    plt.show()
+
+def boxplot(dataframe, fontsize, labelsize, title, colors):
+
+    plt.figure(figsize=(11, 8))
+
+    matplotlib.rcParams.update({'font.size': fontsize})
+    matplotlib.rcParams['font.family'] = "serif"
+    matplotlib.rcParams.update({'xtick.labelsize': labelsize})
+    sns.boxplot(x=dataframe['Parameters'], y=dataframe['value'], palette=colors)
+    plt.xlabel(None)
+    plt.ylabel('DSC')
+    plt.title(title)
+    plt.xticks(rotation=30)
+    plt.ylim(-0.05, 1.05)
     plt.tight_layout()
     plt.show()
 
