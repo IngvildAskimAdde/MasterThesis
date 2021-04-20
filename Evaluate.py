8052739b0903219949dd6150d4e8b758b0b961a3
Ingvild Askim Adde <ingvilad@stud.ntnu.no>
2021-04-20 15:41:21 +0200

    Evaluating

--- Evaluate.py
+++ Evaluate.py
@@ -5,12 +5,12 @@ import numpy as np
 import  matplotlib
 
 #LARC_ID_32 = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/LARC/LARC_ID_32/logs.csv')
-Oxy_ID_21_new = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/Oxy_new/Oxy_ID_21_new/logs.csv')
+#Oxy_ID_21_new = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/Oxy_new/Oxy_ID_21_new/logs.csv')
 #Oxy_ID_8_new = pd.read_csv('/Users/ingvildaskimadde/Desktop/Oxy_ID_8_new/logs.csv')
-#Combined_ID_18_new = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/Combined_new/Combined_ID_18_new/logs.csv')
+Combined_ID_20_new = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/Combined_new/Combined_ID_20_new/logs.csv')
 
-Oxy_ID_21_patient = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/Oxy_new/Oxy_ID_21_new/patient.csv')
-Oxy_ID_21_slice = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/Oxy_new/Oxy_ID_21_new/slice.csv')
+#Oxy_ID_21_patient = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/Oxy_new/Oxy_ID_21_new/patient.csv')
+#Oxy_ID_21_slice = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/Oxy_new/Oxy_ID_21_new/slice.csv')
 #slice_352 = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/LARC/LARC_ID_32/slice_352.csv')
 #slice_256 = pd.read_csv('/Volumes/LaCie/MasterThesis_Ingvild/Experiments/LARC/LARC_ID_32/slice_256.csv')
 #LARC_ID_32_slice = slice_352.append(slice_256)
@@ -96,8 +96,8 @@ def get_data(dataframe, column):
 #median, std = calculate_median(dataframe)
 
 #max_dice, epoch = find_best_epoch(Combined_ID_9)
-max_dice, epoch = find_best_epoch(Oxy_ID_21_new)
-mean, median = get_data(Oxy_ID_21_slice, 'f1_score')
+max_dice, epoch = find_best_epoch(Combined_ID_20_new)
+#mean, median = get_data(Oxy_ID_21_slice, 'f1_score')
 #mean = get_data(LARC_ID_10_slice, 'f1_score')
 
 
