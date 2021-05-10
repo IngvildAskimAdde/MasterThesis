ce499c72da70ae45c007da20bee94531e75ec751
Ingvild Askim Adde <ingvilad@stud.ntnu.no>
2021-05-10 15:21:56 +0200

    Evaluating and post-processing results.

--- post_process.py
+++ post_process.py
@@ -6,19 +6,19 @@ import customize_obj
 
 
 if __name__ == '__main__':
-    output_folder = '/Volumes/LaCie/MasterThesis_Ingvild/Experiments/LARC/LARC_ID_46/' # change this to the folder you want to store the result
-    dataset_file = '/Volumes/LaCie/MasterThesis_Ingvild/HDF5_data/traditionalSplit_LARC_352_MHZScore.h5' # path to the dataset
+    output_folder = '/Volumes/LaCie/MasterThesis_Ingvild/Experiments/LARC/LARC_ID_53/' # change this to the folder you want to store the result
+    dataset_file = '/Volumes/LaCie/MasterThesis_Ingvild/HDF5_data/traditionalSplit_LARC_MHZScore_TS.h5' # path to the dataset
 
-    predicted_h5 = '/Volumes/LaCie/MasterThesis_Ingvild/Experiments/LARC/LARC_ID_46/prediction.014.h5' # the prediction file you want to calculate the dice
+    predicted_h5 = '/Volumes/LaCie/MasterThesis_Ingvild/Experiments/LARC/LARC_ID_53/prediction.039.h5' # the prediction file you want to calculate the dice
 
-    dice_per_slice = output_folder + 'slice.csv'
-    dice_per_patient = output_folder + 'patient.csv'
-    merge_file = output_folder + 'merge_images.h5'
+    dice_per_slice = output_folder + 'slice_256.csv'
+    dice_per_patient = output_folder + 'patient_256.csv'
+    merge_file = output_folder + 'merge_images_256.h5'
 
     customize_obj.H5MetaDataMapping(
         dataset_file,
         dice_per_slice,
-        folds=['val/352'], # change this to ['test'] if you want to calculate the dice of the test prediction
+        folds=['val/256'], # change this to ['test'] if you want to calculate the dice of the test prediction
         fold_prefix='',
         dataset_names=['patient_ids'] #, 'slice_idx']
     ).post_process()
